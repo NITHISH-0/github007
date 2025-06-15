4d3255b81166249005de845ec033b8e805c21073
Nithish Karthikeyan <nithish.karthikeyan@accenture.com>
2025-06-15 10:35:03 +0530

    added one line

--- Myprogram3.cs
+++ Myprogram3.cs
@@ -12,19 +12,19 @@ namespace Method_Function
         static void Main(string[] args)
         {
 
+            Buildrequest("karthi");
+            Buildrequest("nithish");
+            Buildrequest("suren");
+            Buildrequest("sarvesh");
 
-            //Buildrequest("nithish");
-            //Buildrequest("suren");
-            //Buildrequest("sarvesh");
-
-            Buildrequest(10,4);
+            Buildrequest(10, 4);
 
             //Console.WriteLine(Buildrequest(4, 5, 6));
             //Console.WriteLine(Buildrequest(4, 8, 6));
             //Console.WriteLine(Buildrequest(9, 8, 6));
         }
 
-        public static void Buildrequest(int a, int b )
+        public static void Buildrequest(int a, int b)
         {
 
             var result = a + b;
@@ -37,8 +37,8 @@ namespace Method_Function
             Console.WriteLine($"Devision : {result}");
             result = a % b;
             Console.WriteLine($"Persentage : {result}");
-            
-            
+
+
             //Console.WriteLine($"Hi, My name is{name}");
 
             //int c = a + b + d;
